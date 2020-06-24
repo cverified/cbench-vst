521713af6ddc593c215c96a67c0ad67e3aa1e7bf
Andrew W. Appel <appel@princeton.edu>
2020-06-24 09:26:29 -0400

    Started on sqrt3.c benchmark

---
+++ sqrt/sqrt3_f_correct.v
@@ -0,0 +1,38 @@
+From Flocq Require Core Binary.
+From Coquelicot Require Import Coquelicot.
+Require Import Reals Gappa.Gappa_library Psatz.
+Import Defs Raux FLT Generic_fmt Gappa_definitions Binary Ulp.
+Require Import FunInd Recdef.
+
+From compcert.lib Require Import Floats Integers.
+
+Transparent Float32.div Float32.add Float32.cmp.
+
+(* Correctness of floating-point absolute value *)
+Lemma fabs_float32_lemma:
+  forall x: float32,
+  Float32.of_bits (Int.and (Float32.to_bits x) (Int.repr 2147483647)) =
+  Float32.abs x.
+Proof.
+Admitted.
+
+Definition sqrt_approx_f (x: float32) : float32 :=
+  Float32.of_bits
+  (Int.add (Int.shru (Float32.to_bits x) (Int.repr 1))
+     (Int.repr 532676608)).
+
+Definition float32_to_real := Binary.B2R 24 128.
+
+Open Scope R_scope.
+Lemma sqrt_approx_correct:
+ forall x, 
+  Rbasic_fun.Rabs (float32_to_real (sqrt_approx_f x) - R_sqrt.sqrt (float32_to_real x)) <=
+       1 + (1/15) * R_sqrt.sqrt (float32_to_real x).
+Proof.
+Admitted.
+Close Scope R_scope.
+
+
+
+
+
---
+++ sqrt/verif_sqrt3.v
@@ -0,0 +1,32 @@
+Require Import VST.floyd.proofauto.
+Require Import sqrt3.
+Instance CompSpecs : compspecs. make_compspecs prog. Defined.
+Definition Vprog : varspecs. mk_varspecs prog. Defined.
+
+Definition sqrt_approx_f (x: float32) : float32 :=
+  Float32.of_bits
+  (Int.add (Int.shru (Float32.to_bits x) (Int.repr 1))
+     (Int.repr 532676608)).
+
+Definition sqrt_approx_spec :=
+   DECLARE _sqrt_approx
+   WITH x: float32
+   PRE [ tfloat ]
+       PROP () PARAMS (Vsingle x) SEP ()
+    POST [ tfloat ]
+       PROP () RETURN (Vsingle (sqrt_approx_f x)) SEP ().
+
+Definition Gprog : funspecs :=
+         [sqrt_approx_spec].
+
+Lemma body_sqrt_approx:  semax_body Vprog Gprog f_sqrt_approx sqrt_approx_spec.
+Proof.
+start_function.
+forward.
+forward.
+forward.
+forward.
+forward.
+entailer!.
+Qed.
+
